784f0465546680e491b1df031fdb986708b7559e
Steve <steven.g.miller@gmail.com>
2021-02-19 23:43:19 -0500

    removed old comments

--- main.py
+++ main.py
@@ -8,13 +8,9 @@ parser.add_argument("-i", "--input", help="Path to input file", required=True)
 parser.add_argument("-o", "--output", default="output.txt", help="Path to output file")
 args = parser.parse_args()
 
-
-# with open(sys.argv[1], 'r') as my_file:
 with open(args.input) as my_file:
 
     fout = open(args.output, "wt")
-#    filename = my_file.name + "_out.txt"
-#    fout = open(filename, "wt")
 
     for line in my_file:
         fout.write(line.replace('\t', '    '))
